93e0779cc239828fe5d80c46ae6934200769d2ac
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-12-07 02:01:57 +0530

    Store models in their own subfolder

--- multiagent/constants.py
+++ multiagent/constants.py
@@ -2,13 +2,13 @@
 
 # The binary pickle file that will be loaded and used
 # Make this None if you dont want to use an existing model
-MODEL_TO_USE = "mcts/models/model.pkl"
+MODEL_TO_USE = "models/model.pkl"
 
 
 
 # Outputs - you mostly don't want to change these
 
 # The readable model file - this is simply for you to debug stuff
-OUTPUT_MODEL_TEXT_FILE = "mcts/models/model-latest.txt"
+OUTPUT_MODEL_TEXT_FILE = "models/model-latest.txt"
 # The binary model file that you have built; maybe you want to use this
-OUTPUT_MODEL = "mcts/models/model-latest.pkl"
+OUTPUT_MODEL = "models/model-latest.pkl"
