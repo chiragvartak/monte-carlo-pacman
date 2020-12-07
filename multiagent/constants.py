bd3b93a5280617af38460137f73b1d5035147e25
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-12-07 23:24:54 +0530

    Set the model to use to the perfect-model

--- multiagent/constants.py
+++ multiagent/constants.py
@@ -3,9 +3,7 @@
 # The binary pickle file that will be loaded and used
 # Make this None if you dont want to use an existing model
 # MODEL_TO_USE = None
-# MODEL_TO_USE = "models/perfect-model.pkl"
-# MODEL_TO_USE = "models/model-100000.pkl"
-MODEL_TO_USE = "models/model-10000.pkl"
+MODEL_TO_USE = "models/perfect-model.pkl"
 
 
 
